f4d24ed6f8a87e777974b4c994f3916d550b41d3
owenmerry <merryowen@gmail.com>
2023-05-06 19:33:57 +0200

    removed total check

--- app/components/ui/flight-results/flight-results-default.tsx
+++ app/components/ui/flight-results/flight-results-default.tsx
@@ -116,7 +116,7 @@ interface PagingProps {
 
 const Paging = ({ shown = 100, total = 1000, onShowMore = (number: number) => { } }: PagingProps) => {
     return (
-        <>{total} / {shown}
+        <>
             {total > shown ? (
                 <div className='my-4 text-center'>
                     <button className='text-white bg-blue-700 border border-transparent hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 disabled:hover:bg-blue-700 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:disabled:hover:bg-blue-600 focus:!ring-2 group h-min items-center justify-center p-0.5 text-center font-medium focus:z-10 rounded-lg ml-2' onClick={() => onShowMore(shown + 100)}>
