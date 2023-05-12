a2c8c251ead187c93544db802acec234f25d896a
owenmerry <merryowen@gmail.com>
2023-05-12 23:57:50 +0200

    mobile update to seo

--- app/routes/seo.$country.tsx
+++ app/routes/seo.$country.tsx
@@ -25,7 +25,7 @@ export default function SEOAnytime() {
         <div>
           <h2 className='text-3xl mb-6'>Cities in {selectedPlace.name}</h2>
         </div>
-        <div className='grid grid-cols-5'>
+        <div className='grid sm:grid-cols-5 grid-cols-2'>
           {placeList.sort((a, b) => a.name.localeCompare(b.name)).map((place) => {
             if (!(place.type === 'PLACE_TYPE_CITY' && place.parentId === params.country)) return;
 
