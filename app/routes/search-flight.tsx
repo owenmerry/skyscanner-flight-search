960e5d218dc06ab33bb3991b8194ab68db2c40d9
owenmerry <merryowen@gmail.com>
2023-06-07 08:34:31 +0100

    added fix for images to all one file

--- app/routes/search-flight.tsx
+++ app/routes/search-flight.tsx
@@ -1,110 +1,12 @@
-import { useEffect, useState } from "react";
-import type { LoaderFunction } from "@remix-run/node";
-import { useLoaderData, Outlet, useLocation } from "@remix-run/react";
+import { Outlet, useLocation } from "@remix-run/react";
 import { Layout } from "~/components/ui/layout/layout";
-import { HeroPage } from "~/components/ui/hero/hero-page";
-import { getPlaceFromIata } from "~/helpers/sdk/place";
-import { getImagesFromParents } from "~/helpers/sdk/images";
-
-interface Query {
-  from: string;
-  fromIata: string;
-  fromText: string;
-  to: string;
-  toIata: string;
-  toText: string;
-  depart: string;
-  return: string;
-  tripType: string;
-}
-
-export const loader: LoaderFunction = async ({ request, context, params }) => {
-  const apiUrl = process.env.SKYSCANNER_APP_API_URL || "";
-
-  //exit
-  if (!params.from || !params.to)
-    return {
-      apiUrl,
-    };
-
-  //get locations
-  const fromPlace = getPlaceFromIata(params.from);
-  const toPlace = getPlaceFromIata(params.to);
-
-  if (!fromPlace || !toPlace) return {};
-
-  const flightParams: Query = {
-    from: params.from || "",
-    fromIata: fromPlace.iata,
-    fromText: fromPlace.name,
-    to: params.to || "",
-    toIata: toPlace.iata,
-    toText: toPlace.name,
-    depart: params.depart || "",
-    return: params.return || "",
-    tripType: "return",
-  };
-
-  const parentImages = getImagesFromParents(toPlace.entityId);
-
-  console.log("run loader...");
-
-  return {
-    apiUrl,
-    flightParams,
-    parentImages,
-    params,
-  };
-};
 
 export default function SearchFlight() {
-  const {
-    apiUrl,
-    flightParams,
-    parentImages,
-    params,
-  }: {
-    apiUrl: string;
-    flightParams: Query;
-    parentImages: string[];
-  } = useLoaderData();
-  const [query, setQuery] = useState(flightParams);
-  const [image, setImage] = useState(parentImages[0]);
   const { pathname } = useLocation();
-
-  useEffect(() => {
-    const pathSplit = pathname.split("/");
-    const fromPlace = getPlaceFromIata(pathSplit[2]);
-    const toPlace = getPlaceFromIata(pathSplit[3]);
-    const parentImages = getImagesFromParents(toPlace.entityId);
-    setImage(parentImages[0]);
-
-    const flightParams: Query = {
-      from: pathSplit[2] || "",
-      fromIata: fromPlace.iata,
-      fromText: fromPlace.name,
-      to: pathSplit[3] || "",
-      toIata: toPlace.iata,
-      toText: toPlace.name,
-      depart: pathSplit[4] || "",
-      return: pathSplit[5] || "",
-      tripType: "return",
-    };
-    setQuery(flightParams);
-    console.log("run effect...", pathname, params);
-  }, [pathname]);
 
   return (
     <div>
       <Layout selectedUrl="/search-flight">
-        <HeroPage
-          apiUrl={apiUrl}
-          buttonLoading={false}
-          flightDefault={query}
-          backgroundImage={image}
-          key={`hero-${pathname}`}
-        />
-        {pathname}
         <Outlet key={`outlet-${pathname}`} />
       </Layout>
     </div>
--- app/routes/search-flight/$from.$to.$depart.$return.tsx
+++ app/routes/search-flight/$from.$to.$depart.$return.tsx
@@ -1,15 +1,30 @@
-import { useEffect, useState } from 'react';
-import type { LoaderFunction, LoaderArgs } from '@remix-run/node';
-import { FiltersDefault } from '~/components/ui/filters/filters-default';
-import { FlightResultsDefault } from '~/components/ui/flight-results/flight-results-default';
+import { useEffect, useState } from "react";
+import type { LoaderFunction, LoaderArgs } from "@remix-run/node";
+import { FiltersDefault } from "~/components/ui/filters/filters-default";
+import { FlightResultsDefault } from "~/components/ui/flight-results/flight-results-default";
 import { getFlightLiveCreate, getFlightLivePoll } from "~/helpers/sdk/query";
-import { useLoaderData } from '@remix-run/react';
-import { getEntityIdFromIata } from '~/helpers/sdk/place';
-import { Spinner } from 'flowbite-react';
+import { useLoaderData } from "@remix-run/react";
+import { getEntityIdFromIata } from "~/helpers/sdk/place";
+import { Spinner } from "flowbite-react";
+import { getPlaceFromIata } from "~/helpers/sdk/place";
+import { getImagesFromParents } from "~/helpers/sdk/images";
+import { HeroPage } from "~/components/ui/hero/hero-page";
+import { SearchSDK } from "~/helpers/sdk/skyscannerSDK";
 
+interface Query {
+  from: string;
+  fromIata: string;
+  fromText: string;
+  to: string;
+  toIata: string;
+  toText: string;
+  depart: string;
+  return: string;
+  tripType: string;
+}
 
 export const loader = async ({ params }: LoaderArgs) => {
-  const apiUrl = process.env.SKYSCANNER_APP_API_URL || '';
+  const apiUrl = process.env.SKYSCANNER_APP_API_URL || "";
 
   //exit
   if (!params.from || !params.to) return;
@@ -18,34 +33,68 @@ export const loader = async ({ params }: LoaderArgs) => {
   const from = getEntityIdFromIata(params.from);
   const to = getEntityIdFromIata(params.to);
 
+  //query
+  const fromPlace = getPlaceFromIata(params.from);
+  const toPlace = getPlaceFromIata(params.to);
+  if (!fromPlace || !toPlace) return {};
+  const flightParams: Query = {
+    from: params.from || "",
+    fromIata: fromPlace.iata,
+    fromText: fromPlace.name,
+    to: params.to || "",
+    toIata: toPlace.iata,
+    toText: toPlace.name,
+    depart: params.depart || "",
+    return: params.return || "",
+    tripType: "return",
+  };
+
+  //images
+  const parentImages = getImagesFromParents(toPlace.entityId);
+
   //get search
   const flightSearch = await getFlightLiveCreate({
     apiUrl,
     query: {
       from,
       to,
-      depart: params.depart || '',
+      depart: params.depart || "",
       return: params.return,
-      tripType: 'return',
-    }
+      tripType: "return",
+    },
   });
 
   return {
     apiUrl,
     params,
     flightSearch,
-  }
-
+    flightParams,
+    parentImages,
+  };
 };
 
 export default function Search() {
-  const { flightSearch, apiUrl } = useLoaderData<typeof loader>();
+  const {
+    flightSearch,
+    apiUrl,
+    flightParams,
+    parentImages,
+  }: {
+    apiUrl: string;
+    flightParams: Query;
+    parentImages: string[];
+    flightSearch: SearchSDK | { error: string };
+  } = useLoaderData();
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState(flightSearch);
   const [filters, setFilters] = useState({});
   const [showFilters, setShowFilters] = useState(false);
-  const [error, setError] = useState('error' in flightSearch ? flightSearch.error : '');
-  const sessionToken = 'sessionToken' in search ? search.sessionToken : '';
+  const [error, setError] = useState(
+    "error" in flightSearch ? flightSearch.error : ""
+  );
+  const [query, setQuery] = useState(flightParams);
+  const [image, setImage] = useState(parentImages[0]);
+  const sessionToken = "sessionToken" in search ? search.sessionToken : "";
 
   useEffect(() => {
     setLoading(true);
@@ -59,47 +108,68 @@ export default function Search() {
       wait: 1,
     });
 
-    if ('error' in res) {
+    if ("error" in res) {
       setError(res.error);
       runPoll();
 
       return;
-    };
-    setError('');
+    }
+    setError("");
 
-    if (
-      res.status === "RESULT_STATUS_INCOMPLETE"
-    ) {
+    if (res.status === "RESULT_STATUS_INCOMPLETE") {
       setSearch(res);
       runPoll();
     } else {
       setSearch(res);
       setLoading(false);
     }
-  }
+  };
 
   return (
     <div>
-      <div className='bg-white dark:bg-gray-900'>
-        <div className='md:flex justify-between mx-4 max-w-screen-xl bg-white dark:bg-gray-900 xl:p-9 xl:mx-auto'>
+      <HeroPage
+        apiUrl={apiUrl}
+        buttonLoading={false}
+        flightDefault={query}
+        backgroundImage={image}
+      />
+      <div className="bg-white dark:bg-gray-900">
+        <div className="md:flex justify-between mx-4 max-w-screen-xl bg-white dark:bg-gray-900 xl:p-9 xl:mx-auto">
           <div
-            className='md:hidden border-2 border-slate-100 py-4 px-4 rounded-lg mb-2 cursor-pointer dark:text-white dark:border-gray-800'
+            className="md:hidden border-2 border-slate-100 py-4 px-4 rounded-lg mb-2 cursor-pointer dark:text-white dark:border-gray-800"
             onClick={() => setShowFilters(!showFilters)}
           >
-            {showFilters ? 'Hide Filters' : 'Show Filters'}
+            {showFilters ? "Hide Filters" : "Show Filters"}
           </div>
-          <div className={`${showFilters ? '' : 'hidden'} xl:w-[400px] md:block max-w-none`}>
+          <div
+            className={`${
+              showFilters ? "" : "hidden"
+            } xl:w-[400px] md:block max-w-none`}
+          >
             <FiltersDefault onFilterChange={(filters) => setFilters(filters)} />
           </div>
-          <div className='w-full md:ml-2'>
-            {error !== '' ? <div className='dark:text-white'> {error}
-            </div> : <>
-              {loading ? <div className='text-center p-5 mb-4 text-slate-400 bg-slate-50 rounded-xl dark:bg-gray-800'><Spinner className='mr-2' /> Loading More Prices & Flights...</div> : ''}
-              <FlightResultsDefault flights={'error' in search ? undefined : search} filters={filters} />
-            </>}
+          <div className="w-full md:ml-2">
+            {error !== "" ? (
+              <div className="dark:text-white"> {error}</div>
+            ) : (
+              <>
+                {loading ? (
+                  <div className="text-center p-5 mb-4 text-slate-400 bg-slate-50 rounded-xl dark:bg-gray-800">
+                    <Spinner className="mr-2" /> Loading More Prices &
+                    Flights...
+                  </div>
+                ) : (
+                  ""
+                )}
+                <FlightResultsDefault
+                  flights={"error" in search ? undefined : search}
+                  filters={filters}
+                />
+              </>
+            )}
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
