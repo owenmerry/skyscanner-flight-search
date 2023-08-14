f5272694df3201859ec9b3c521fe925f49571b23
owenmerry <merryowen@gmail.com>
2023-08-14 17:07:09 +0100

    added function for Search link

--- app/helpers/sdk/skyscanner-website.tsx
+++ app/helpers/sdk/skyscanner-website.tsx
@@ -10,3 +10,10 @@ export const getSkyscannerLink = (query: QueryPlace, bookingId?: string) => {
     query.return ? `/${getBookingDate(query.return)}` : ""
   }${bookingId ? `/config/${encodeURIComponent(bookingId)}` : ""}`;
 };
+export const getSkyscannerSearchLink = (query: QueryPlace) => {
+  return `https://skyscanner.net/g/referrals/v1/flights/day-view/?origin=${
+    query.from.iata
+  }&destination=${query.to.iata}&outboundDate=${query.depart}${
+    query.return ? `&inboundDate=${query.return}` : ""
+  }`;
+};
