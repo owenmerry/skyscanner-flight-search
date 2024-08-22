5a9bc62060cd53f3a3ae4a7a8f7a577262f8498c
owenmerry <merryowen@gmail.com>
2024-08-22 18:10:51 +0100

    added update for competitor

--- app/components/section/competitor-check/competitor-check.tsx
+++ app/components/section/competitor-check/competitor-check.tsx
@@ -33,9 +33,9 @@ export const CompetitorCheck = ({
     <div className="relative z-20">
       {query && skyscannerSearch && skyscannerSearch.cheapest[0] ? (
         <div className="">
-          {kiwiSearch ? (
+          {kiwiSearch && kiwiSearch.data[0] ? (
             <div className="mb-2 p-4 text-white bg-emerald-700 rounded-md text-lg relative hover:bg-emerald-600 scale-100 hover:scale-105">
-              {kiwiSearch.data[0].fare.adults <
+              {kiwiSearch.data[0]?.fare.adults <
               Number(skyscannerSearch.cheapest[0].price.replace("£", "")) ? (
                 <div className="text-center absolute -top-2 right-2">
                   <div className="py-1 px-3 -mt-4 md:inline-block text-sm text-emerald-800 bg-emerald-100 rounded dark:bg-emerald-200 dark:text-emerald-800">
@@ -47,13 +47,13 @@ export const CompetitorCheck = ({
               )}
               <div className="text-sm">Kiwi</div>
               <div className="font-bold">
-                £{kiwiSearch.data[0].fare.adults.toFixed(2)}
+                £{kiwiSearch.data[0]?.fare.adults.toFixed(2)}
               </div>
               <div>
                 <a
                   className="text-xs hover:underline -mt-2"
                   target="_blank"
-                  href={kiwiSearch.data[0].deep_link}
+                  href={kiwiSearch.data[0]?.deep_link}
                 >
                   (See Price)
                 </a>
@@ -66,8 +66,8 @@ export const CompetitorCheck = ({
             <div className="mb-2 p-4 text-white bg-primary-700 rounded-md text-lg relative hover:bg-primary-600 scale-100 hover:scale-105">
               {kiwiSearch &&
               skyscannerSearch.cheapest[0] &&
-              kiwiSearch.data[0].fare.adults >
-                Number(skyscannerSearch.cheapest[0].price.replace("£", "")) ? (
+              kiwiSearch.data[0]?.fare.adults >
+                Number(skyscannerSearch.cheapest[0]?.price.replace("£", "")) ? (
                 <div className="text-center absolute -top-2 right-2">
                   <div className="py-1 px-3 -mt-4 inline-block text-sm text-primary-800 bg-primary-100 rounded dark:bg-primary-200 dark:text-primary-800">
                     Cheapest
@@ -78,13 +78,13 @@ export const CompetitorCheck = ({
               )}
               <div className="text-sm">Skyscanner</div>
               <div className="font-bold">
-                {skyscannerSearch.cheapest[0].price}
+                {skyscannerSearch.cheapest[0]?.price}
               </div>
               <div>
                 <a
                   className="text-xs hover:underline -mt-2"
                   target="_blank"
-                  href={skyscannerSearch.cheapest[0].deepLink}
+                  href={skyscannerSearch.cheapest[0]?.deepLink}
                 >
                   (See Price)
                 </a>
