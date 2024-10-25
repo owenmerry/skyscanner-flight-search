6451d1c78cef33bd2fe496d55e532596a2073f22
owenmerry <merryowen@gmail.com>
2024-10-25 12:11:47 +0100

    removed logos from flowbite

--- app/components/ui/header/header-default.tsx
+++ app/components/ui/header/header-default.tsx
@@ -125,13 +125,9 @@ export const HeaderDefault = ({
       <nav className="relative z-40 bg-white border-gray-200 drop-shadow-md px-4 lg:px-6 py-2.5 dark:bg-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            />
+            
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flights
+              flights.owenmerry.com
             </span>
           </Link>
           <div className="flex items-center lg:order-2">
