198c52568cc7b2a27551e9ba0fa86f29b2af032d
owenmerry <merryowen@gmail.com>
2024-11-23 23:43:59 -0500

    added pop ups on trip

---
+++ app/components/section/trip-timeline/drawer-day-view.tsx
@@ -0,0 +1,86 @@
+import { Drawer } from "@mui/material";
+import type { ReactNode } from "react";
+import { useEffect, useState } from "react";
+
+interface DayViewDrawer {
+  children: ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const DayViewDrawer: React.FC<DayViewDrawer> = ({ children, isOpen, onClose }) => {
+  const [open, setOpen] = useState(isOpen);
+  type ToggleDrawer = (
+    open: boolean
+  ) => (event: React.KeyboardEvent | React.MouseEvent) => void;
+
+  const toggleDrawer: ToggleDrawer = (open) => () => {
+    if(open === false){
+      onClose();
+    }
+    setOpen(open);
+  };
+
+  useEffect(() => {
+    setOpen(isOpen);
+  }, [isOpen]);
+
+  if (!children) return "";
+
+  return (
+    <div>
+      <Drawer
+        PaperProps={{
+          sx: {},
+        }}
+        anchor="right"
+        open={open}
+        onClose={toggleDrawer(false)}
+        ModalProps={{ keepMounted: true }} // Better open performance on mobile
+      >
+        <div className="dark">
+          <div className="min-h-screen w-screen md:w-[70vw] dark:bg-gray-900">
+            <Header onClose={() => setOpen(false)}>Trip Details</Header>
+            <div className="pb-16 dark:text-white">{children}</div>
+          </div>
+        </div>
+      </Drawer>
+    </div>
+  );
+};
+
+const Header: React.FC<{ onClose: () => void; children: ReactNode }> = ({
+  onClose,
+  children,
+}) => (
+  <div className="flex items-center p-4 md:p-5 border-b rounded-t dark:border-gray-800 shadow-md dark:bg-slate-900 sticky w-full top-0 z-10">
+    <button
+      type="button"
+      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+      data-modal-hide="static-modal"
+      onClick={onClose}
+    >
+      <svg
+        className="w-6 h-6 text-gray-800 dark:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M5 12h14M5 12l4-4m-4 4 4 4"
+        />
+      </svg>
+      <span className="sr-only">Close modal</span>
+    </button>
+    <h3 className="text-xl flex-1 font-semibold text-gray-900 dark:text-white text-center">
+      {children}
+    </h3>
+  </div>
+);
--- app/components/section/trip-timeline/trip-timeline.component.tsx
+++ app/components/section/trip-timeline/trip-timeline.component.tsx
@@ -1,156 +1,106 @@
 import moment from "moment";
+import React, { useState } from "react";
 import { FaCarSide, FaHotel, FaPlaneDeparture } from "react-icons/fa";
 import { MdDateRange } from "react-icons/md";
+import { DayViewDrawer } from "./drawer-day-view";
+
+interface TripPlan {
+  days: {
+    date: string;
+    events: {
+      type: "flight" | "hotel" | "car" | "activity";
+      name?: string;
+    }[];
+  }[];
+}
 
 interface TripTimelineProps {}
 export const TripTimeline = ({}: TripTimelineProps) => {
-  const trip = {
+  const [dayView, setDayView] = useState<string>();
+  const trip: TripPlan = {
     days: [
-      { date: "2025-02-01" },
-      { date: "2025-02-02" },
-      { date: "2025-02-03" },
-      { date: "2025-02-04" },
+      { date: "2025-02-01", events: [{ type: "flight" }] },
+      {
+        date: "2025-02-02",
+        events: [{ type: "activity", name: "Scuba Diving" }],
+      },
+      {
+        date: "2025-02-03",
+        events: [{ type: "activity", name: "Scuba Diving" }],
+      },
+      { date: "2025-02-04", events: [{ type: "flight" }] },
     ],
   };
   return (
     <div>
       <div className="px-4 py-4">
+        <div onClick={() => setDayView(undefined)}>Close</div>
         <ol className="relative">
-          <li className="ml-8">
-            <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-3 ring-6 ring-white dark:ring-gray-700 dark:bg-blue-600 z-20 shadow-md">
-              <FaPlaneDeparture />
-            </span>
-            <div>Day 1</div>
-          </li>
-          {/* info from */}
-          <li className="relative ml-8">
-            <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
-            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-              {moment(trip.days[0].date).format("dddd, Do MMMM")}
-            </h3>
-            <div className="text-xs text-slate-400">
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="inline-flex items-center px-3 py-1 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                href={"/"}
-              >
-                <FaCarSide className="mr-2" /> Car Hire
-              </a>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="inline-flex items-center px-3 py-1 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                href={"/"}
-              >
-                <FaHotel className="mr-2" /> Hotel
-              </a>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="inline-flex items-center px-3 py-1 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                href={"/"}
-              >
-                <FaPlaneDeparture className="mr-2" /> Flight
-              </a>
-            </div>
-          </li>
-          {/* duration */}
-          <li className="py-5 ml-8 relative">
-            <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
-            <div className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
-                Nothing here
-              </div>
-            </div>
-            <div className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
-                Nothing here
-              </div>
-            </div>
-            <div className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
-                Nothing here
-              </div>
-            </div>
-            <div className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
-                Nothing here
-              </div>
-            </div>
-            <div className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
-                Nothing here
-              </div>
-            </div>
-          </li>
-          {/* to date with icon */}
-          <li className="ml-8">
-            <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-3 ring-6 ring-white dark:ring-gray-700 dark:bg-blue-600 z-20 shadow-md">
-              <MdDateRange />
-            </span>
-            <div>Day 2</div>
-          </li>
-          <li className="relative ml-8">
-            <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
-            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-              {moment(trip.days[1].date).format("dddd, Do MMMM")}
-            </h3>
-          </li>
-          <li className="py-5 ml-8 relative">
-            <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
-            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
-                Nothing here
-              </div>
-            </time>
-          </li>
-          <li className="ml-8">
-            <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-3 ring-6 ring-white dark:ring-gray-700 dark:bg-blue-600 z-20 shadow-md">
-              <MdDateRange />
-            </span>
-            <div>Day 3</div>
-          </li>
-          <li className="relative ml-8">
-            <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
-            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-              {moment(trip.days[2].date).format("dddd, Do MMMM")}
-            </h3>
-          </li>
-          <li className="py-5 ml-8 relative">
-            <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
-            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
-                Nothing here
-              </div>
-            </time>
-          </li>
-          <li className="ml-8">
-            <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-3 ring-6 ring-white dark:ring-gray-700 dark:bg-blue-600 z-20 shadow-md">
-              <FaPlaneDeparture />
-            </span>
-            <div>Day 4</div>
-          </li>
-          <li className="relative ml-8">
-            <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
-            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-              {moment(trip.days[3].date).format("dddd, Do MMMM")}
-            </h3>
-            <div className="text-xs text-slate-400">
-              <span className="mr-2">Add a stop over:</span>
-              {[1, 2, 3, 5].map((days, key) => (
-                <a
-                  key={key}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="inline-flex items-center px-3 py-1 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                  href={"/"}
-                >
-                  + {days} day{days === 1 ? "" : "s"}
-                </a>
-              ))}
-            </div>
-          </li>
+          {trip.days.map((day, key) => {
+            return (
+              <React.Fragment key={day.date}>
+                {/* day details */}
+                <li className="ml-8" onClick={() => setDayView(day.date)}>
+                  <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-3 ring-6 ring-white dark:ring-gray-700 dark:bg-blue-600 z-20 shadow-md">
+                    <MdDateRange />
+                  </span>
+                  <div>Day {key + 1} - {dayView}</div>
+                </li>
+                {/* info */}
+                <li className="relative ml-8">
+                  <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
+                  <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
+                    {moment(trip.days[0].date).format("dddd, Do MMMM")}
+                  </h3>
+                </li>
+                {/* events */}
+                <li className="py-2 ml-8 relative">
+                  <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
+                  {day.events.map((event, key) => {
+                    return (
+                      <React.Fragment key={key}>
+                        <div className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                          <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
+                            {event.type === "activity"
+                              ? event.name
+                              : event.type}
+                          </div>
+                        </div>
+                      </React.Fragment>
+                    );
+                  })}
+                </li>
+                <DayViewDrawer isOpen={dayView !== undefined} onClose={() => setDayView(undefined)}>
+                  <div className="px-4 py-4">
+                    <ol className="relative">
+                      <li className="ml-8">
+                        <span className="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -start-3 ring-6 ring-white dark:ring-gray-700 dark:bg-blue-600 z-20 shadow-md">
+                          <MdDateRange />
+                        </span>
+                        <div>Day {key}</div>
+                      </li>
+                      <li className="py-2 ml-8 relative">
+                        <div className="absolute -top-[4px] -left-8 w-2 h-[calc(100%+8px)] bg-blue-800 rounded-lg py-4 z-10"></div>
+                        {day.events.map((event, key) => {
+                          return (
+                            <React.Fragment key={key}>
+                              <div className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                                <div className="inline-block bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded dark:bg-blue-900 dark:text-blue-300">
+                                  {event.type === "activity"
+                                    ? event.name
+                                    : event.type}
+                                </div>
+                              </div>
+                            </React.Fragment>
+                          );
+                        })}
+                      </li>
+                    </ol>
+                  </div>
+                </DayViewDrawer>
+              </React.Fragment>
+            );
+          })}
         </ol>
       </div>
     </div>
