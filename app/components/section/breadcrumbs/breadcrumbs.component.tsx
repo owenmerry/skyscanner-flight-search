3fb13e24254f2b0365182b1915f83b30cacd98a4
owenmerry <merryowen@gmail.com>
2025-04-12 20:51:24 +0100

    updated breadcrumb to lowercase

--- app/components/section/breadcrumbs/breadcrumbs.component.tsx
+++ app/components/section/breadcrumbs/breadcrumbs.component.tsx
@@ -1,5 +1,3 @@
-import { Wrapper } from "~/components/ui/wrapper/wrapper.component";
-
 interface BreadcrumbsProps {
   items?: { link?: string; name: string; nofollow?: boolean }[];
 }
@@ -49,7 +47,7 @@ export const Breadcrumbs = ({ items = [] }: BreadcrumbsProps) => {
                 {item.link ? (
                   <a
                     rel={item.nofollow === true ? "nofollow" : ""}
-                    href={item.link}
+                    href={item.link.toLowerCase()}
                     className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
                   >
                     {item.name}
